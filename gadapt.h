74fbe69486d2c0094b9c8dcca467643840098d7b
Stepainpy <stepa2014vlg@gmail.com>
2025-05-17 17:55:41 +0300

    remove include macros

--- gadapt.h
+++ gadapt.h
@@ -1,25 +1,19 @@
 #ifndef GENERIC_ADAPTOR_H
 #define GENERIC_ADAPTOR_H
 
-/* Stack */
-#ifdef GAD_INCLUDE_STACK
 #include "gda.h"
+#include "gheap.h"
+
+/* Stack */
 
 #define gstk_push gda_push
 #define gstk_pop  gda_pop
 #define gstk_top  gda_last
 
-#endif /* GAD_INCLUDE_STACK */
-
 /* Priority queue */
-#ifdef GAD_INCLUDE_PR_QUEUE
-#include "gda.h"
-#include "gheap.h"
 
 #define gprq_push ghp_push
 #define gprq_pop  ghp_pop
 #define gprq_peek gda_front
-
-#endif /* GAD_INCLUDE_PR_QUEUE */
 
 #endif /* GENERIC_ADAPTOR_H */
